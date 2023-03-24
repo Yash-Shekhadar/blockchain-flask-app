5e48d032653205e6d717798ff580daeb0b099dd6
Yash Shekhadar <53370886+Yash-Shekhadar@users.noreply.github.com>
2023-03-24 19:14:05 -0400

    Trial df read

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ app = Flask(__name__)
 print("Its me, hi, I am the problem its me!")
 urllist = ['https://drive.google.com/file/d/1fGCocjbtGwEYBYmIiKn7IgRdqD45toL7/view?usp=share_link', 'https://drive.google.com/file/d/1eyAwWBHtXGghYmZn_tKNZ53cVury1jT1/view?usp=share_link', 'https://drive.google.com/file/d/1WSrIw5tZIDbUQm30-TK09Ru2NpvzL2cZ/view?usp=share_link']
 filenames = ['encounter.csv', 'loitering.csv', 'port.csv']
+data = []
 
 for i in range(len(filenames)):
-    urllib.request.urlretrieve(urllist[i], filenames[i])
+    with urllib.request.urlopen(urllist[i]) as f:
+        data = f.read().decode('utf-8')
+    print(f"Downloaded {filenames[i]}.")
 
-df_e = pd.read_csv("./encounter.csv")
-df_l = pd.read_csv("./loitering.csv")
-df_p = pd.read_csv("./port.csv")
+df_e = pd.read_csv(data[0])
+df_l = pd.read_csv(data[1])
+df_p = pd.read_csv(data[2])
 print(len(df_e), len(df_l), len(df_p))
 
 @app.route('/')
