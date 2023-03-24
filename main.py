bd86ea1e20a063b84e2e8f8d47ec40ba46e431f8
Yash Shekhadar <53370886+Yash-Shekhadar@users.noreply.github.com>
2023-03-24 18:00:11 -0400

    Data reading snippet added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
-
+import urllib.request
+import pandas as pd
 app = Flask(__name__)
 
 
@@ -12,3 +12,14 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+    
+    urllist = ['https://drive.google.com/file/d/1fGCocjbtGwEYBYmIiKn7IgRdqD45toL7/view?usp=share_link', 'https://drive.google.com/file/d/1eyAwWBHtXGghYmZn_tKNZ53cVury1jT1/view?usp=share_link', 'https://drive.google.com/file/d/1WSrIw5tZIDbUQm30-TK09Ru2NpvzL2cZ/view?usp=share_link']
+    filenames = ['encounter.csv', 'loitering.csv', 'port.csv']
+
+    for i in range(len(filenames)):
+        urllib.request.urlretrieve(urllist[i], filenames[i])
+
+    df_e = pd.read_csv("./encounter.csv")
+    df_l = pd.read_csv("./loitering.csv")
+    df_p = pd.read_csv("./port.csv")
+    
