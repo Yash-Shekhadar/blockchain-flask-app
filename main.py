48d07f2c3e8b6214822032678eb876637ebbfb7e
Yash Shekhadar <53370886+Yash-Shekhadar@users.noreply.github.com>
2023-03-25 03:20:49 -0400

    Api testing

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask
-
+import pandas as pd
 
 app = Flask(__name__)
+
+df_e = pd.read_csv('./mock_data/encounter_final.csv')
+df_s = pd.read_csv('./mock_data/ship.csv')
+df_p = pd.read_csv('./mock_data/port.csv')
+
+
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return len(df_e)
 
 
 if __name__ == '__main__':
