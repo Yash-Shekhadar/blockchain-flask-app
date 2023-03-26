5ed64671110f8349f74f4f238e557ce208a6a0dc
Yash Shekhadar <53370886+Yash-Shekhadar@users.noreply.github.com>
2023-03-25 22:45:49 -0400

    Final changes

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import pandas as pd
 import random
 # from datetime import datetime
 import math
+
+
 app = Flask(__name__)
 
+# Reading the datasets into dataframes
 df_e = pd.read_csv('./mock_data/encounter_final.csv')
 df_s = pd.read_csv('./mock_data/ship.csv')
 df_p = pd.read_csv('./mock_data/port.csv')
 
 
-
+# API to check the existence of a ship 
 @app.route('/data/<mmsi>')
 def checkExistence(mmsi):
     if int(mmsi) in df_s.v1MMSI.values:
@@ -18,6 +21,7 @@ def checkExistence(mmsi):
     else:
         return {"mmsi":999999999}
     
+# API to check if a given ship has been involved in illegal encountering activity
 @app.route('/encounter/<mmsi>/<start>/<end>')
 def encounterCheck(mmsi, start, end):
 
@@ -25,8 +29,6 @@ def encounterCheck(mmsi, start, end):
         return {'iuuInvolved': True}
     
     else:
-        # temp = df_e.query("@start <= endTime")
-        # flaggedMMSI = temp.query("startTime <= @end")['v1MMSI'].values.tolist()
         flaggedMMSI = df_e.query("@start <= endTime and startTime <= @end")['v1MMSI'].values.tolist()
         print(flaggedMMSI)
         if int(mmsi) in flaggedMMSI:
@@ -35,6 +37,7 @@ def encounterCheck(mmsi, start, end):
             return {'iuuInvolved': False}
         
 
+# API to validate the location of a ship and make sure that it is not involved in fishing in illegal waters
 @app.route('/location/<mmsi>/<lat>/<lon>')
 def locationCheck(mmsi, lat, lon):
 
@@ -50,6 +53,7 @@ def locationCheck(mmsi, lat, lon):
         # reqDist/earthRadius
         distRad = 101/6371
 
+        # Mathematics to simulate a live coordinate of the ship
         latRad = math.radians(float(lat))
         lonRad = math.radians(float(lon))
         newLatRad = math.asin(math.sin(latRad) * math.cos(distRad) + math.cos(latRad) * math.sin(distRad) * math.cos(0))
@@ -62,8 +66,6 @@ def locationCheck(mmsi, lat, lon):
                 'actualLon': str(newLon),
                 'nearby': False
                 }
-
-        
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
