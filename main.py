760dae1617e2ca151d42897254b0520291a5ff83
Yash Shekhadar <53370886+Yash-Shekhadar@users.noreply.github.com>
2023-03-25 21:04:39 -0400

    location check mock api added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 import pandas as pd
 import random
-from datetime import datetime
+# from datetime import datetime
+import math
 app = Flask(__name__)
 
 df_e = pd.read_csv('./mock_data/encounter_final.csv')
@@ -26,12 +27,44 @@ def encounterCheck(mmsi, start, end):
     else:
         # temp = df_e.query("@start <= endTime")
         # flaggedMMSI = temp.query("startTime <= @end")['v1MMSI'].values.tolist()
-        flaggedMMSI = df_e.query("@start <= endTime and startTime <= @end").values.tolist()
+        flaggedMMSI = df_e.query("@start <= endTime and startTime <= @end")['v1MMSI'].values.tolist()
         print(flaggedMMSI)
         if int(mmsi) in flaggedMMSI:
             return {'iuuInvolved': True}
         else:
             return {'iuuInvolved': False}
+        
+
+@app.route('/location/<mmsi>/<lat>/<lon>')
+def locationCheck(mmsi, lat, lon):
+
+    if random.random() < 0.95:
+        return {'sentLat': lat,
+                'sentLon': lon,
+                'actualLat': lat,
+                'actualLon': lon,
+                'nearby': True
+                }
+    
+    else:
+        # reqDist/earthRadius
+        distRad = 101/6371
+
+        latRad = math.radians(float(lat))
+        lonRad = math.radians(float(lon))
+        newLatRad = math.asin(math.sin(latRad) * math.cos(distRad) + math.cos(latRad) * math.sin(distRad) * math.cos(0))
+        newLonRad = math.asin(math.sin(lonRad) * math.cos(distRad) + math.cos(lonRad) * math.sin(distRad) * math.cos(0))
+        newLat = math.degrees(newLatRad)
+        newLon = math.degrees(newLonRad)
+        return {'sentLat': lat,
+                'sentLon': lon,
+                'actualLat': str(newLat),
+                'actualLon': str(newLon),
+                'nearby': False
+                }
+
+        
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
     
