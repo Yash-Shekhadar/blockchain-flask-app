a45aa63eece01e90ab88c19ed05369d870e5c95f
Yash Shekhadar <53370886+Yash-Shekhadar@users.noreply.github.com>
2023-03-25 20:21:11 -0400

    Encounter api working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import pandas as pd
-
+import random
+from datetime import datetime
 app = Flask(__name__)
 
 df_e = pd.read_csv('./mock_data/encounter_final.csv')
@@ -15,9 +16,22 @@ def checkExistence(mmsi):
         return {"mmsi": int(mmsi)}
     else:
         return {"mmsi":999999999}
+    
+@app.route('/encounter/<mmsi>/<start>/<end>')
+def encounterCheck(mmsi, start, end):
 
-
+    if random.random() >= 0.95:
+        return {'iuuInvolved': True}
+    
+    else:
+        # temp = df_e.query("@start <= endTime")
+        # flaggedMMSI = temp.query("startTime <= @end")['v1MMSI'].values.tolist()
+        flaggedMMSI = df_e.query("@start <= endTime and startTime <= @end").values.tolist()
+        print(flaggedMMSI)
+        if int(mmsi) in flaggedMMSI:
+            return {'iuuInvolved': True}
+        else:
+            return {'iuuInvolved': False}
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
     
-
